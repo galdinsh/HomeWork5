04e55f473ebf1bee76fb136ebfa3ccc1fe6ad39e
Vladislav <galdinsh77@mail.ru>
2023-03-23 12:05:18 +0500

    Задача№34

---
+++ Task34/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000); // [100, 1000]
+}
+
+int CountNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 1)
+            count++;
+    }
+    return count;
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Количество четных чисел в массиве: {CountNumbers(array)}");
