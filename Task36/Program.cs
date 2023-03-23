c187a1ef24dfb838f0d6f536fcf463291d800c37
Vladislav <galdinsh77@mail.ru>
2023-03-23 12:20:07 +0500

    Задача№36

---
+++ Task36/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10, 11); // [-10, 100]
+}
+
+int SumOdd (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+            sum = sum + array[i];
+    }
+    return sum;
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Сумма элементов на нечетных позициях в массиве: {SumOdd(array)}");
